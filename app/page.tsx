ee01554b937e96810287a4c82a13b509ff2e7b8b
NicolasLi <linze201@gmail.com>
2023-11-23 11:49:56 +0800

    lazy load 3rd-party library

--- app/page.tsx
+++ app/page.tsx
@@ -9,20 +9,31 @@ import { Metadata } from 'next';
 // import HeavyComponent from './components/HeavyComponent';
 import { useState } from 'react';
 import dynamic from 'next/dynamic';
-const HeavyComponent = dynamic(() => import('./components/HeavyComponent'), {
-  // set server side rendering to false
-  // when using browser API, set to false to avoid error
-  ssr: false,
-  loading: () => <p>Loading...</p>,
-});
+// const HeavyComponent = dynamic(() => import('./components/HeavyComponent'), {
+//   // set server side rendering to false
+//   // when using browser API, set to false to avoid error
+//   ssr: false,
+//   loading: () => <p>Loading...</p>,
+// });
+// import _ from 'lodash';
 
 export default function Home() {
-  const [isVisible, setIsVisible] = useState(false);
+  // const [isVisible, setIsVisible] = useState(false);
   // const session = await getServerSession(authOptions);
   return (
     <main className='relative h-screen'>
-      <button onClick={() => setIsVisible(true)}>Show</button>
-      {isVisible && <HeavyComponent />}
+      <button
+        onClick={async () => {
+          const _ = (await import('lodash')).default;
+          const users = [{ name: 'c' }, { name: 'b' }, { name: 'a' }];
+          const sorted = _.orderBy(users, ['name']);
+          console.log(sorted);
+        }}
+      >
+        Sort
+      </button>
+      {/* <button onClick={() => setIsVisible(true)}>Show</button> */}
+      {/* {isVisible && <HeavyComponent />} */}
       {/* <Image src={cat} alt='cat' /> */}
       {/* <Image
         src='https://bit.ly/react-cover'
